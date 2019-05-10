a44778be4e6375f93fed95c54832feff419951ce
Jamie Vullo <jvullo@protonmail.com>
2019-05-10 14:50:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require "pry"
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style, language|
+    
+    language.each do |attribute, type|
+      #binding.pry
+      #new_hash[attribute] = |type|
+      
+      
+        
+      end
+    end
+  end  
+  new_hash
 end
